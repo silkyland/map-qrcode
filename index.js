b798adfa8bddf89e7abf7fc4f91f02053c7d6d8b
Bundit Nuntates <silkyland@gmil.com>
2020-08-18 13:51:18 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const QRCode = require("qrcode");
 const csv = require("csv-parser");
 const fs = require("fs");
+const mdMaster = require("./files/md-master");
 
+var txt = "";
 fs.createReadStream("files/form.csv")
   .pipe(csv())
   .on("data", (row) => {
+    txt += `| ${row.baan} | ${row.locationNameTh} | ${row.locationNameEn} | ${
+      row.uuid
+    } | ![${
+      row.locationNameTh
+    }](https://raw.githubusercontent.com/silkyland/map-qrcode/master/qrcode/${encodeURIComponent(
+      row.locationNameTh
+    )}.png) |\n`;
     QRCode.toFile(`./qrcode/${row.locationNameTh}.png`, row.uuid, {
       width: 512,
     });
     // QRCode.toFile();
   })
   .on("end", () => {
+    fs.writeFileSync("README.md", mdMaster(txt), {
+      encoding: "utf8",
+      flag: "w",
+    });
     console.log("CSV file successfully processed");
   });
 
